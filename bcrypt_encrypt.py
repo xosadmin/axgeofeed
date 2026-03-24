c2d159a61188e3110456e5d6a9171d650dcca8de
xosadmin <9164356+xosadmin@users.noreply.github.com>
2026-03-24 23:11:21 +0800

    Fix bcrypt hashed code calculator

--- bcrypt_encrypt.py
+++ bcrypt_encrypt.py
@@ -2,6 +2,6 @@ from utils.bcryptworks import encrypt_password
 import base64
 
 plaintext = input("Enter your plaintext: ")
-encrypted = encrypt_password(plaintext).decode('utf-8')
+encrypted = encrypt_password(plaintext)
 hashed = base64.b64encode(encrypted.encode('utf-8')).decode('utf-8')
 print(hashed)
